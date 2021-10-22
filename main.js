81cf13732bdd0f7b4ab2dd13e0f4ed4a12950172
saijailona <82865009+saijailona@users.noreply.github.com>
2021-10-22 11:50:08 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const apiURL = 'https://api.tvmaze.com/search/shows?';
+
+const hae = function(event) {
+    event.preventDefault();
+    $("#tulos").empty();
+$.getJSON(apiURL + $(this).serialize(), function(response){
+    $("#tulos").append('<h1>Tulokset</h1>');
+    $.each(response, function(indeksi, sarja){
+        console.log(sarja.show);
+        $("#tulos").append(`<article>
+    <h2>${sarja.show.name}</h2>
+    <p>${sarja.show.genres}</p>
+    <a href="${sarja.show.officialSite ? sarja.show.officialSite : sarja.show.url}">Linkki kotisivulle</a>
+    <figure>
+      <img src="${sarja.show.image ? sarja.show.image.medium : 'tile.png'}" alt="${sarja.show.name}">
+      <figcaption>${sarja.show.name}</figcaption>
+    </figure>
+    <p>${sarja.show.summary}</p>
+  </article>`);
+        });
+    });
+};
+
+$('form').submit(hae);
